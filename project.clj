f790bbae9ffefcbb2836d870fe8cb2fe2458b35e
nick rothwell <nick@cassiel.com>
2014-04-08 11:14:34 +0100

    "(no message)"

--- project.clj
+++ project.clj
@@ -1,8 +1,23 @@
+(def LAYOUTS {:PREVIEW {:x 20 :y 20 :w 800 :h 600}})
+
+(defn screen [tag] (reduce #(str %1 "." %2)
+                           (map #(% (tag LAYOUTS)) [:x :y :w :h])))
+
 (defproject eu.cassiel/hc2 "0.1.0-SNAPSHOT"
   :description "TODO"
   :url "TODO"
   :license {:name "TODO: Choose a license"
             :url "http://choosealicense.com/"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]]
-                   :source-paths ["dev"]}})
+  :plugins [[lein-midje "3.1.1"]
+            [lein-field "0.3.0-SNAPSHOT"]]
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.4"]
+                                  [midje "1.5.1"]]
+                   :source-paths ["dev"]}}
+  :field-arguments {:noHistory 0
+                    ;; :timer.interval 0.033
+                    :editorFont "Courier"
+                    :editorFontSize 12
+                    :editorTabSize 4
+                    :rect ~(screen :PREVIEW)
+                    :field.scratch "hc2.field"})
--- src/eu/cassiel/hc2.clj
+++
@@ -1,3 +0,0 @@
-(ns eu.cassiel.hc2)
-
-
