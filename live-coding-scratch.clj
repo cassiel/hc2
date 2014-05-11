5a313cf6a82861b34547fdc9d8054945a58f0b43
nick rothwell <nick@cassiel.com>
2014-05-11 15:59:30 +0200

    "(no message)"

--- live-coding-scratch.clj
+++ live-coding-scratch.clj
@@ -1,3 +1,7 @@
 (ns eu.cassiel.hc2.main)
 
-(defn live-jumper [] "xxxxjump")
+(defn live-jumper-1 [] "jump")
+(defn live-jumper-2 [] "spiral")
+(defn live-jumper-3 [] "rebound")
+(defn live-jumper-4 [] "expand")
+(defn live-jumper-5 [] "swing")
