c53256b86f24ac618670ec21048a6133e8ecbd45
nick (alibeykoy) <nick@cassiel.com>
2014-11-02 21:22:18 +0000

    "(no message)"

--- live-coding-scratch.clj
+++ live-coding-scratch.clj
@@ -5,3 +5,9 @@
 (defn live-jumper-3 [] "rebound")
 (defn live-jumper-4 [] "expand")
 (defn live-jumper-5 [] "swing")
+
+(defn live-jumper-1 [] "BISH")
+(defn live-jumper-2 [] "BASH")
+(defn live-jumper-3 [] "BOSH")
+(defn live-jumper-4 [] "BING")
+(defn live-jumper-5 [] "BONG")
