58233ff5e1664979be72f45d89c6b017b01ddb14
nick rothwell <nick@cassiel.com>
2014-11-07 13:16:45 +0000

    "(no message)"

--- live-coding-scratch.clj
+++ live-coding-scratch.clj
@@ -1,14 +1,32 @@
+;; To connect:
+;;    View->Connections
+;;    Add Connection
+;;    Clojure (remote nREPL)
+;;    bakirkoy.cassiel.com:9000
+
+;; Then:
+
+
+;; Evaluate this first:
 (ns eu.cassiel.hc2.main)
 
+
+;; Fire when ready! These take effect in e_knees only:
 (defn live-jumper-1 [] "jump")
 (defn live-jumper-2 [] "spiral")
 (defn live-jumper-3 [] "rebound")
 (defn live-jumper-4 [] "expand")
 (defn live-jumper-5 [] "swing")
 
-(defn live-jumper-1 [] "BISH")
-(defn live-jumper-2 [] "BASH")
-(defn live-jumper-3 [] "BOSH")
-(defn live-jumper-4 [] "BING")
-(defn live-jumper-5 [] "BONG")
 
+;; Fire this statement to see the current phrase (if any):
+(__examine)
+
+
+;; Notes for Nick:
+;; On bakirkoy.cassiel.com
+;;    screen ~/tcpforward/tcpforward -v -l 0.0.0.0:9000 -l 0.0.0.0:9001
+;; On localhost, when Field running:
+;;    ~/tcpforward/tcpforward -v -c bakirkoy.cassiel.com:9001 -c localhost:4555
+;; Remote nREPL connection point:
+;;    bakirkoy.cassiel.com:9000
